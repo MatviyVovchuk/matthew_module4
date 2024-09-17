fbe128c1f2ac06e1591367e71a3c4fc044f84074
mvovchuk <m.vovchuk@edu.internetdevels.com>
2024-09-17 17:34:56 +0300

    task/347565: The “add table” button was added, validation and calculations were edited.

--- web/modules/custom/matthew_tables/src/Entity/MatthewTablesEntry.php
+++ web/modules/custom/matthew_tables/src/Entity/MatthewTablesEntry.php
@@ -46,7 +46,7 @@ class MatthewTablesEntry extends ContentEntityBase {
   /**
    * {@inheritdoc}
    */
-  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type): array {
     $fields = parent::baseFieldDefinitions($entity_type);
 
     $fields['year'] = BaseFieldDefinition::create('integer')
